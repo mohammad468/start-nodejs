d285ffcbf6e416fea9059be5b501bc00d65e17a5
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-25 19:40:49 +0330

    find id in url and show this object in array

--- server.js
+++ server.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const students = [
+  { id: "1", userName: "mohammad", age: 20 },
+  { id: "2", userName: "ali", age: 19 },
+  { id: "3", userName: "hossein", age: 19 },
+  { id: "4", userName: "amir", age: 17 },
+  { id: "5", userName: "nikzad", age: 19 },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/students", (req, res) => {
-  res.send([
-    { id: 1, userName: "mohammad", age: 20 },
-    { id: 2, userName: "ali", age: 19 },
-    { id: 3, userName: "hossein", age: 19 },
-    { id: 4, userName: "amir", age: 17 },
-    { id: 5, userName: "nikzad", age: 19 },
-  ]);
+  res.send(students);
 });
 
-app.get("/students/:userName", (req, res) => {
-  res.send(req.params);
+app.get("/students/:id", (req, res) => {
+  const student = students.find((item) => item.id === req.params.id);
+  if (student) {
+    res.send(student);
+  } else {
+    res.send("not found");
+  }
 });
 
-app.get("/students/:userName/:family", (req, res) => {
-  res.send(req.query);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

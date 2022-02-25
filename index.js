5b61080754087f592b427417de11947242053269
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-25 17:05:04 +0330

    write hello world in lestening sever

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log("new request");
+  res.write("hello world");
+  res.end();
 });
 server.listen(3000);
 console.log("Server listening in port 3000");

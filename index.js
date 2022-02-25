c50808233d48bb5b27351c64d17f328b17b221cb
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-25 17:10:58 +0330

    create write in my url and run in chrome

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log("new request");
-  res.write("hello world");
-  res.end();
+  if (req.url === "/") {
+    res.write("hello world");
+    res.end();
+  } else if (req.url === "/api/customers") {
+    res.write("customers list");
+    res.end();
+  }
 });
 server.listen(3000);
 console.log("Server listening in port 3000");

3fe952101ecdcf16960ce7ae296c4125d9a79a2d
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-25 17:03:06 +0330

    new request in create server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require("http");
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  console.log("new request");
+});
 server.listen(3000);
 console.log("Server listening in port 3000");

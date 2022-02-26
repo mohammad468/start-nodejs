80dfd39be69720ad60a643a306e3778d0fc3a465
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-26 17:12:48 +0330

    middleware for download files

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
 
 // ?my middleWares
 app.use((req, res, next) => {

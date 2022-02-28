e6288aa0918ab47205267b2ba6720cf6ba5a9bbb
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-28 16:13:07 +0330

    promise

--- promise.js
+++ promise.js
@@ -1,17 +1,17 @@
 const p = new Promise((resolve, reject) => {
   // ....
-  //   setTimeout(() => {
-  //     resolve({
-  //       id: 1,
-  //       name: "mohammad",
-  //     });
-  //   }, 2000);
-  reject(new Error("Error"));
+  setTimeout(() => {
+    resolve({
+      id: 1,
+      name: "mohammad",
+    });
+  }, 2000);
+  //   reject(new Error("Error"));
 });
 
-// p.then((item) => {
-//   console.log(item);
-// });
-p.catch((item) => {
-  console.log("cant fetch data");
+p.then((item) => {
+  console.log(item);
 });
+// p.catch((item) => {
+//   console.log("cant fetch data");
+// });
